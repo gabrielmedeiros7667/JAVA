ace223b42f03e5cb20e444bf4c3896240a828a7f
Gabriel Medeiros <gabrielmedeiros7667@gmail.com>
2026-09-23 09:31:37 -0300

    litas

---
+++ lista/src/ex002.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ex002 {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        double nota1, nota2, nota3, média;
+
+        System.out.print("Digite sua primeira nota: ");
+        nota1 = entrada.nextInt();
+        System.out.print("Digite sua segunda nota: ");
+        nota2 = entrada.nextInt();
+        System.out.print("Digite sua terceira nota: ");
+        nota3 = entrada.nextInt();
+        média = nota1 + nota2 + nota3 / 3;
+        System.out.print("Sua média é: " + média);
+    }
+}
